99ba555c040738926183fd598a7c3396b1eb9d52
Juergen Fesslmeier <jfesslmeier@gmail.com>
2013-10-18 13:48:08 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FoundryRails::Application.config.secret_token = 'f363c42a125ec1a1a165ade68cc0698b1a614cec58d0f4f1b6147de4fd9f5531773b067e5c6c88b32e33c05db920399795ba9e619687c8c59cdefe86d31e4057'
